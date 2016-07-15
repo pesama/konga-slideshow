1065d8eb399d0eb6edd42a862ec10638297282d5
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-15 09:37:56 +0200

    Almost done.

--- src/app.js
+++ src/app.js
@@ -65,335 +65,341 @@ angular.module('konga-slideshow', [
 }])
 .run(['$rootScope', ($rootScope) => {
     script.init([
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'disruptive-start',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'lean-explanation',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'tdd-explanation',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'dev-explanation',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'prod-explanation',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'disruptive-end',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-context',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-government',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-architecture',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-development',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-deployment',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-closure',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-qa',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'toc-finish',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-dev-growing',
-            data: {},
-            options: {
-                auto_next: true,
-                auto_next_duration: 0
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'timeline-2010',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_IMPRESS_PREVIOUS
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-dev-growing-2',
-            data: {},
-            options: {
-                auto_next: true,
-                auto_next_duration: 0
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'timeline-2012',
-            data: {}
-        },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'disruptive-start',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'lean-explanation',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'tdd-explanation',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'dev-explanation',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'prod-explanation',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'disruptive-end',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-context',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-government',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-architecture',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-development',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-deployment',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-closure',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-qa',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'toc-finish',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-dev-growing',
+        //     data: {},
+        //     options: {
+        //         auto_next: true,
+        //         auto_next_duration: 0
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'timeline-2010',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_PREVIOUS
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-dev-growing-2',
+        //     data: {},
+        //     options: {
+        //         auto_next: true,
+        //         auto_next_duration: 0
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'timeline-2012',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'context-rampagejs'
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'context'
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'timeline-2014',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-api-awareness',
+        //     data: {},
+        //     options: {
+        //         auto_next: true,
+        //         auto_next_duration: 500
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'timeline-hide',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-apis-social',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-apis-info',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-apis-storage',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-apis-platform',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'context-apis-misc',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'context-puzzle'
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-init-metadata',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-data',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-relations',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-custom',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-generate',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-model',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-annotations',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-maven',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'architecture-overview'
+        //     },
+        //     options: {
+        //         auto_next: true,
+        //         auto_next_duration: 1500
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-metadata-rotate',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-init-engine',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'architecture-engine'
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-data',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-existing',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-custom',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-federate',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-model',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'architecture-overview'
+        //     },
+        //     options: {
+        //         auto_next: true,
+        //         auto_next_duration: 1500
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'architecture-engine-rotate',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_GOTO,
+        //     data: {
+        //         target: 'government'
+        //     }
+        // },
+        // {
+        //     type: Script.STEP_IMPRESS_NEXT
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'government-planning',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'government-users',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'government-presupuestor',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'government-csc',
+        //     data: {}
+        // },
+        // {
+        //     type: Script.STEP_DISPATCH_EVENT,
+        //     event: 'government-bbva',
+        //     data: {}
+        // },
         {
             type: Script.STEP_IMPRESS_GOTO,
             data: {
-                target: 'context-rampagejs'
+                target: 'examples-overview'
             }
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'context'
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'timeline-2014',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-api-awareness',
-            data: {},
-            options: {
-                auto_next: true,
-                auto_next_duration: 500
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'timeline-hide',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-apis-social',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-apis-info',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-apis-storage',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-apis-platform',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'context-apis-misc',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'context-puzzle'
-            }
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-init-metadata',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-data',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-relations',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-custom',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-generate',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-model',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-annotations',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-maven',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'architecture-overview'
-            },
-            options: {
-                auto_next: true,
-                auto_next_duration: 1500
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-metadata-rotate',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-init-engine',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'architecture-engine'
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-data',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-existing',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-custom',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-federate',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-model',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'architecture-overview'
-            },
-            options: {
-                auto_next: true,
-                auto_next_duration: 1500
-            }
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'architecture-engine-rotate',
-            data: {}
-        },
-        {
-            type: Script.STEP_IMPRESS_GOTO,
-            data: {
-                target: 'government'
-            }
-        },
-        {
-            type: Script.STEP_IMPRESS_NEXT
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'government-planning',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'government-users',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'government-presupuestor',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'government-csc',
-            data: {}
-        },
-        {
-            type: Script.STEP_DISPATCH_EVENT,
-            event: 'government-bbva',
-            data: {}
         },
         {
             type: Script.STEP_IMPRESS_NEXT
